47f393b661a7cf8c08795ae09b48d9359efb81c2
Abid Ali <mohdabidaliusa@gmail.com>
2025-05-16 23:53:36 -0700

    Added comments.

--- src/main/java/com/abid/QRCodeGenerator.java
+++ src/main/java/com/abid/QRCodeGenerator.java
@@ -1,5 +1,9 @@
 package com.abid;
 
 public interface QRCodeGenerator {
+    /*
+     todo instead of taking content as string take is an Object.
+     Object should be dynamic, using dynamic object.
+     */
     void generateQRCode(String content, int width, int height, String filePath);
 }
